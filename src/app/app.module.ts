f1005fa85bcdd8fe8f0042f2f767dfd0d9eac6bd
Omar Rahman <omarrahman96@gmail.com>
2018-02-04 18:05:51 +0000

    Convert existing webpage to angular. Convert single page app into an angular SPA.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ModalComponent } from './modal/modal.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MapComponent } from './map/map.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
+const appRoutes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'map', component:MapComponent},
+  {path:'reviews', component:ReviewsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ModalComponent,
+    HomeComponent,
+    AboutComponent,
+    MapComponent,
+    ReviewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule.forRoot(),
+    ModalModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit,TemplateRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
+ 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+  ngOnInit() {
+  }
+
+}
